3ef9d6e54d17389530c0d2f7af75dc40a47518b2
Tim <timevko@gmail.com>
2014-04-14 18:21:24 -0400

    responsive view improved, alert added when user hits 20 items, stupid js removed, 20 new items

--- practice/assets/js/main.js
+++ practice/assets/js/main.js
@@ -1,18 +1,3 @@
-//Main Javascripts
-
-//if you have to use jQuery, use the wrapper below. It wraps jQuery in its own object so that it's syntax won't be confused with any other scripts
-
-(function($ , window , undefined) {
-    $(function() {
-        function setWindowHeight(){
-            var windowHeight = window.innerHeight;
-            document.body.style.height = windowHeight + "px";
-        }
-        window.addEventListener("resize",setWindowHeight,false);
-        window.addEventListener("load",setWindowHeight,false);
-    });
-})(jQuery , window );
-
 
 //if you need anything other than core functionality, redownload from http://modernizr.com/download/
 
